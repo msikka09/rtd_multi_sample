a3ee87a3a6091c7b4d5b527e11079f5dd2db8451
U-JPL\msikka <msikka@MT-405425.jpl.nasa.gov>
2025-04-21 13:50:02 -0700

    updated rtd theme

---
+++ README.md
@@ -0,0 +1,7 @@
+# GRACE_FO_JPL_L2_document
+
+GRACE-FO L2 processing Document for JPL center taken from here and converted to readthedocs format. https://podaac.jpl.nasa.gov/gravity/gracefo-documentation
+Original document was Prepared by: Dah-Ning Yuan, JPL
+Version 1.0
+Date: May 28, 2019
+JPL D-103921 for original pdf document
--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,18 @@
 
 import os
+import sphinx_rtd_theme
 
-project = 'MyProject'
+project = 'GRACE-FO'
+copyright = '2025, GRACE-FO'
+release = '2025'
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['nbsphinx','sphinx.ext.autosectionlabel']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
 
 # Detect RTD project to set master_doc
 rtd_project = os.environ.get('READTHEDOCS_PROJECT')
@@ -12,3 +23,20 @@ elif rtd_project == 'l2':
     master_doc = 'L2/index'
 else:
     master_doc = 'index'
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_logo = "_static/GRACE_FO_logo.png"
+html_theme_options = {
+    'logo_only': True,
+	'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
